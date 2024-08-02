aa32095f3083eb15effd68cb829be3ca2ddc8152
Qomunar8 <qomuna_illahia_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 15:09:05 +0700

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+public class Hello{
+    public static void main(String[] args) {
+    
+        String nama="Qomuna"; 
+        String asalkota="Tulungagung";
+        int usia=16;
+        double beratbadan=80.00;
+        float tinggibadan=180.0f;
+        char jenisKelamin='L';
+        boolean status= false;
+
+        System.out.println("Nama: "+nama);
+        System.out.println("usia: "+usia);
+        System.out.println("beratbadan: "+beratbadan);
+        System.out.println("tinggibadan: "+tinggibadan);
+        System.out.println("jeniskelamin: "+jenisKelamin);
+        System.out.println("status: "+status);
+    }
+}   
