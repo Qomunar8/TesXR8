404e8306c3e2cff9988025ba9be2d742eb3f0427
Qomunar8 <qomuna_illahia_33rpl@student.smktelkom-mlg.sch.id>
2024-08-23 10:35:11 +0700

    Add files via upload

--- BukuTester.java
+++ BukuTester.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class BukuTester {
     public static void main(String[] args) {
-       Buku sejarah = new Buku(13579, "sejarahfisika", "andi",2012);
+        Scanner in = new Scanner(System.in);
+        System.out.println("inputkan nama pengarang: ");
+        String pengarang = in.nextLine();
+        System.out.println("inputkan judul buku: ");
+        String judul = in.nextLine();
+        System.out.println("inputkan id buku: ");
+        int id = in.nextInt();
+        System.out.println("masukkan tahun terbit: ");
+        int tahunTerbit = in.nextInt();
+
+        Buku kamus = new Buku(id, judul, pengarang, tahunTerbit);
+        System.out.println("==================================");
+        System.out.println(kamus.pengarang);
+        System.out.println(kamus.judul);
+        System.out.println(kamus.id);
+        System.out.println(kamus.tahunTerbit);
+
+       /*Buku sejarah = new Buku(13579, "sejarahfisika", "andi",2012);
        Buku babi = new Buku(12345, "babialbino", "made",2010);
        Buku lautan = new Buku(35678, "lautanapi", "lisa",2008);
        Buku neraka = new Buku(98765, "nerakajahanam", "fajar",2020);
@@ -10,7 +29,7 @@ public class BukuTester {
        System.out.println(babi.id+babi.judul+babi.pengarang+babi.tahunTerbit);
        System.out.println(lautan.id+lautan.judul+lautan.pengarang+lautan.tahunTerbit);
        System.out.println(neraka.id+neraka.judul+neraka.pengarang+neraka.tahunTerbit);
-       System.out.println(cerita.id+cerita.judul+cerita.pengarang+cerita.tahunTerbit);
+       System.out.println(cerita.id+cerita.judul+cerita.pengarang+cerita.tahunTerbit);/* */
        
        
     }
