e8963b6f4611b14899d85d3c3d9e702821fd9433
Qomunar8 <qomuna_illahia_33rpl@student.smktelkom-mlg.sch.id>
2024-10-08 00:22:07 +0700

    Add files via upload

---
+++ Teacher.java
@@ -0,0 +1,29 @@
+public class Teacher extends Person {
+    protected String subject;
+
+    public String getSubject() {
+        return this.subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public Teacher(){
+        super();
+        subject = "";
+    }
+
+    public Teacher(String name, int age, String subject){
+        super(name, age);
+        this.subject = subject;
+    }
+
+    public void print(){
+        super.print();
+        System.out.println("subjek: " + subject);
+    }
+
+
+    
+}
